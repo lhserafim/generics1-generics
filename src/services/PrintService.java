3b05d87c35a091efe18aad001e110836ab40c05f
Luiz <lhserafim@gmail.com>
2020-05-18 07:55:02 -0300

    Projeto Criado

---
+++ src/services/PrintService.java
@@ -0,0 +1,16 @@
+package services;
+
+public class PrintService {
+
+    public void addValue(Object value) {
+
+    }
+
+    public Object first() {
+
+    }
+
+    public void print() {
+        System.out.println("Alguma coisa");
+    }
+}
